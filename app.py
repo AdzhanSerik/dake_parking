6bc3891cc93bd5dfa37ee8a92bf58fe0094cf012
AdzhanSerik <adzhan.serik@gmail.com>
2024-11-07 16:35:45 +0500

    Update app configuration with host and port for deployment

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
